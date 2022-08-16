705ef5df524f7e9028df8fe970d98cd0769862f8
ecometto@hotmail.com <ecometto@hotmail.com>
2022-08-16 08:37:15 -0300

    updating

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// CONFIGURACIÓN THEME 
 const botonTheme = document.querySelector('.onOff')
 const ballTheme = document.querySelector('.ball')
 
@@ -17,19 +18,34 @@ botonTheme.addEventListener('click', () => {
     }
 })
 
+
+// CONFIGURACIÓN CARRUSEL 
 const cards = document.querySelectorAll('.carousel-item')
 const secciones = document.querySelectorAll('section')
 
-
-cards.forEach((card, index)=>{
+cards.forEach((card, index) => {
     secciones[index].style.display = "none"
 
-    card.addEventListener('click',()=>{
-        secciones.forEach(secc=>{
+    card.addEventListener('click', () => {
+        secciones.forEach(secc => {
             secc.style.display = "none"
         })
         secciones[index].style.display = "block"
     })
 
     secciones[0].style.display = "block"
+})
+
+
+// CONFIGURACIONES VARIAS 
+const linkToSkill = document.querySelectorAll('.linkToSkill')
+
+linkToSkill.forEach(link => {
+    addEventListener('click', (e) => {
+        e.preventDefault()
+        secciones.forEach((seccion) => {
+            seccion.style.display = "none"
+        })
+        secciones[2].style.display = "block"
+    })
 })
